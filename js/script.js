1156d18caf5902df759ce66c0b948204f4369f5a
abhishek <abhiskg20@gmail.com>
2022-09-04 20:01:37 +0600

    Initial Setup

---
+++ js/script.js
@@ -0,0 +1,14 @@
+"use strict";
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
+const API_KEY = "c221f15c0b15ddd4e2e35e75238d7f79";
+const loadTemperature = (city) => __awaiter(void 0, void 0, void 0, function* () {
+    const res = yield fetch(``);
+});
---
+++ src/script.ts
@@ -0,0 +1,5 @@
+const API_KEY = "c221f15c0b15ddd4e2e35e75238d7f79";
+
+const loadTemperature = async (city: string) => {
+  const res = await fetch(``);
+};
